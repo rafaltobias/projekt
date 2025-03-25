18abab1cb3aedb6b322ff5da556bfa4d584fb4e3
Rafaltobias <kraskiewiczrafal@gmail.com>
2025-03-25 17:14:53 +0100

    rate limit added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit'); 
 const app = express();
 const PORT = 3000;
 const db = require('./src/databse.js');
@@ -8,6 +9,14 @@ const statsController = require('./src/controllers/statsController');
 
 app.use(express.static('public'));
 app.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+// Apply rate limiting to all requests
+app.use(limiter);
 
 app.post('/track', visitController.trackVisit);
 app.get('/api/stats', statsController.getStats);
